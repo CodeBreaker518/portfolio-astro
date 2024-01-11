8292f182ae51a8637e684974fa014787006ea466
Diego Pérez <diegoperezperez518@gmail.com>
2024-01-11 14:55:28 -0600

    fix: error deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,4 @@ import node from '@astrojs/node'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
 })
