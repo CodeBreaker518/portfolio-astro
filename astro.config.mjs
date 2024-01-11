7fd4f7e1ca2952a2e86ec41fa3bd0d9e04cd0bd1
Diego Pérez <diegoperezperez518@gmail.com>
2024-01-11 15:48:04 -0600

    feat: removed resend and added formspree

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,8 @@ import node from '@astrojs/node'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 })
